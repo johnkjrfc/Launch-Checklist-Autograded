38880150975c466ccc1c2bae742f9edc4bfb087f
johnkjrfc <johnkjrfc@gmail.com>
2023-12-13 16:01:32 -0600

    working on submit listener

--- script.js
+++ script.js
@@ -2,15 +2,22 @@
 
 window.addEventListener("load", function() {
 
-    let listedPlanets;
-    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
-    listedPlanetsResponse.then(function (result) {
-        listedPlanets = result;
-        console.log(listedPlanets);
-    }).then(function () {
-        console.log(listedPlanets);
-        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-    })
+    // let listedPlanets;
+    // // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    // let listedPlanetsResponse;
+    // listedPlanetsResponse.then(function (result) {
+    //     listedPlanets = result;
+    //     console.log(listedPlanets);
+    // }).then(function () {
+    //     console.log(listedPlanets);
+    //     // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    // })
+    //uncomment at part 3
     
+    //add event listener for submit button and use formSubmission
+    const submitForm = document.getElementById("formSubmit");
+    submitForm.addEventListener("click", function(event) {
+        formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+        console.log("click received")
+    });
  });
--- scriptHelper.js
+++ scriptHelper.js
@@ -17,12 +17,47 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
     */
  }
  
+ //validateInput() should take in a string as a parameter and return "Empty", "Not a Number", or "Is a Number" as appropriate, and Add an alert at form submission to notify the user that all fields are required. 
  function validateInput(testInput) {
-    
+    if (testInput.trim() === "") {
+        return "Empty";
+    }
+   if (isNaN(testInput)) {
+        return "Not a Number";
+   }
+   return "Is a Number";
  }
  
+ //this will take in a document parameter and strings representing the pilot, co-pilot, fuel level, and cargo mass.
+ //use validateInput to verify inputs 
  function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    event.preventDefault();
+
+    const pilotName = document.getElementByID("pilotName").value;
+    const copilotName = document.querySelector('input[name="copilotName"]');
+    const fuelLevel = document.querySelector('input[name="fuelLevel"]');
+    const cargoMass = document.querySelector('input=[name="cargoMass"]');
+    //array of the input values
+    const inputFields = [pilotName, copilotName, fuelLevel, cargoMass];
     
+
+    for (let i = 0; i < inputFields.length; i++) {
+        const inputValue = inputFields[i];
+        const validationStatus = validateInput(inputValue);
+
+        if (validationStatus === "Empty") {
+            alert("All fields are required!");
+            break;
+        } else if (validationStatus === "Not a Number" && (i === 2 || i ===3)) {
+            alert("Fuel Level and/or Cargo Mass must be valid numbers");
+            break;
+        } else if (validationStatus === "Is a Number" && (i === 0 || i === 1)) {
+            alert("Pilot Name and/or Copilot names are invalid")
+            break;
+        }
+        
+    }
+
  }
  
  async function myFetch() {
